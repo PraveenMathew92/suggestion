2d9823e0a48c28ae2f4fe517f169ee5ea0d7c447
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2018-07-18 23:32:26 +0530

    refactoring the code

--- src/main/kotlin/com/kotlin/kotlindemo/service/PairingMatrixService.kt
+++ src/main/kotlin/com/kotlin/kotlindemo/service/PairingMatrixService.kt
@@ -10,13 +10,12 @@ class PairingMatrixService {
     fun getSuggestion(pairingMatrixData: Mono<List<PairingMatrixData>>): Flux<Pair<String, String>> {
         return pairingMatrixData
                 .map(this::pairingDays)
-                .map { it() }
                 .flatMapMany { Flux.fromIterable(it) }
 
     }
 
-    private fun pairingDays(p: List<PairingMatrixData>): () -> List<Pair<String, String>> = {
-        p.fold(emptyList<String>()) { acc, p -> acc + listOf(p.pair1, p.pair2) }
+    private fun pairingDays(p: List<PairingMatrixData>): List<Pair<String, String>> {
+        return p.fold(emptyList<String>()) { acc, (pair1, pair2) -> acc + listOf(pair1, pair2) }
                 .distinct()
                 .map { m -> Pair(m, p.filter { d -> d.contains(m) }.sortedWith(compareBy { it.days })) }
                 .map { Pair(it.first, it.second.first().getPair(it.first)) }
